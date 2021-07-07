2abfed4289e4b21f32e25c756c6084411a46760e
edwsilme <eesilva2@gmail.com>
2021-07-06 22:15:59 -0500

    Add CRUD Usuario

---
+++ WebApplication1/Controllers/CloseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CloseController : Controller
+    {
+        // GET: Close
+        public ActionResult Logoff()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Login", "Acceso");
+        }
+    }
+}
