e7f23a28907ac8a1bc64b51814ddda9a4a4e971c
edwsilme <eesilva2@gmail.com>
2021-07-06 21:45:41 -0500

    code

---
+++ WebApplication1/Filters/VerificaSesion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Controllers;
+
+namespace WebApplication1.Filters
+{
+    public class VerificaSesion : ActionFilterAttribute
+    {
+        private TBL_USURAIO oUsuario;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                base.OnActionExecuting(filterContext);
+
+                oUsuario = (TBL_USURAIO)HttpContext.Current.Session["User"];
+
+                if (oUsuario == null)
+                {
+                    if (filterContext.Controller is AccesoController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("/Acceso/Login");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                filterContext.Result = new RedirectResult("~/Acceso/Login");
+            }
+        }
+    }
+}
